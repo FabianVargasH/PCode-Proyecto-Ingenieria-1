35e50587b918dd694d6bcde7b0fd78c851e2d2bb
Dionsx <168503089+Dionsx@users.noreply.github.com>
2025-08-04 22:06:35 -0600

    Resolviendo conflicto en backend/server.js combinando rutas y conexión a MongoDB

--- Comunidad_Conectada_Codigo/backend/server.js
+++ Comunidad_Conectada_Codigo/backend/server.js
@@ -1,4 +1,3 @@
-
 // Cargar variables de entorno
 require('dotenv').config();
 
@@ -10,26 +9,28 @@ const app = express();
 // Middlewares
 app.use(express.json()); // para interpretar JSON 
 
-//Importación de rutas
-const eventosRoutes = require ('./routes/eventos');
+// Importación de rutas
+const eventosRoutes = require('./routes/eventos');
 const anunciosRoutes = require('./routes/anuncios');
+const registroRoutes = require('./routes/registro_routes');
 
 // Uso de las rutas 
 app.use('/api/anuncios', anunciosRoutes);
 app.use('/api/eventos', eventosRoutes);  
+app.use('/api/registro', registroRoutes);
 
 // Puerto
 const port = process.env.PORT || 3000;
 
 // Conexión a MongoDB Atlas
 mongoose.connect(process.env.MONGODB_URI)
-  .then(() => console.log(' Conectado a MongoDB Atlas'))
-  .catch((err) => console.error(' Error al conectar con MongoDB:', err));
+  .then(() => console.log('Conectado a MongoDB Atlas'))
+  .catch((err) => console.error('Error al conectar con MongoDB:', err));
 
 // Iniciar servidor
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`); 
-}); 
+});
 
 
 
